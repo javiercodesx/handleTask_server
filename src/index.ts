b0ec84623c0e351a781f27488d3d641d9841461f
javierspr <javxdev@gmail.com>
2024-10-05 14:52:17 -0300

    add colors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import server from './server'
+import color from 'colors'
 
 const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-    console.log(`REST API FROM PORT ${port}`)
+    console.log( color.bgGreen(` REST API FROM PORT ${port} `) )
 })
