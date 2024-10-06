f83883ce3ac221156051d89b096f729eaa0e662e
javierspr <javxdev@gmail.com>
2024-10-06 20:31:02 -0300

    add ProjectController

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from "express"
+
+export class ProjectController {
+    
+    static getAllProjects = async (req: Request, res: Response) => {
+        res.send('All the projects')
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { connectDB } from './config/db'
+import projectRoutes from './routes/projectRoutes'
 
 connectDB()
 
 const app = express()
 
+app.use('/api/projects', projectRoutes)
 
 export default app
