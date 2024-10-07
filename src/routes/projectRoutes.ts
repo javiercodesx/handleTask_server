07aa9668101ae7c93ade2eeecf5e296625ade1f0
javierspr <javxdev@gmail.com>
2024-10-07 14:51:11 -0300

    add createProject validation

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+    body('projectName')
+        .notEmpty().withMessage('The project must have a project name'),
+    body('clientName')
+        .notEmpty().withMessage('The project must have a client name'),
+    body('description')
+        .notEmpty().withMessage('The project must have a description'),
+    ProjectController.createProject
+)
 router.get('/', ProjectController.getAllProjects)
 
 export default router
