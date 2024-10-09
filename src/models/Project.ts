5c9c6ee6db883d8489189a0d25247afb688d2e5a
javierspr <javxdev@gmail.com>
2024-10-09 16:27:24 -0300

    add relationship between Project and Task. add task status

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,9 +1,11 @@
-import mongoose, {Schema, Document}  from "mongoose";
+import mongoose, {Schema, Document, PopulatedDoc, Types}  from "mongoose";
+import { Itask } from "./Task";
 
 export interface IProject extends Document  {
     projectName: string,
     clientName: string,
-    description: string
+    description: string,
+    tasks: PopulatedDoc<Itask & Document>[]
 }
 
 export const ProjectSchema : Schema =  new Schema({
@@ -21,8 +23,14 @@ export const ProjectSchema : Schema =  new Schema({
         type: String,
         required: true,
         trim: true
-    }
-  });
+    },
+    tasks: [
+        {
+            type: Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+  }, {timestamps: true});
 
   const Project = mongoose.model<IProject>('Project', ProjectSchema)
   export default Project
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,8 +1,19 @@
-import mongoose, {Schema, Document}  from "mongoose";
+import mongoose, {Schema, Document, Types}  from "mongoose";
+
+const taskStatus = {
+    PENDING: 'pending',
+    ON_HOLD: 'onHold',
+    IN_PROGRESS: 'inProgress',
+    UNDER_REVIEW: 'underReview',
+    COMPLETED: 'completed'
+} as const
+
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
 
 export interface Itask extends Document  {
     name: string,
-    description: string
+    description: string,
+    project: Types.ObjectId
 }
 
 export const TaskSchema : Schema =  new Schema({
@@ -15,8 +26,17 @@ export const TaskSchema : Schema =  new Schema({
         type: String,
         required: true,
         trim: true
+    },
+    project: {
+        type: Types.ObjectId,
+        ref: 'Project'
+    },
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING
     }
-  });
+  }, {timestamps: true});
 
   const Project = mongoose.model<Itask>('Task', TaskSchema)
   export default Project
