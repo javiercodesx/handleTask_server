e530d78085ad2c3cd5dab2863b73be790166e754
javierspr <javxdev@gmail.com>
2024-10-12 21:06:23 -0300

    add IProject to Request interface

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,27 +1,17 @@
 import type { Request, Response } from 'express';
-import Project from '../models/Project';
 import Task from '../models/Task';
 
 export class TaskController {
     static createTask = async (req: Request, res: Response) => {
-        const { projectId } = req.params
-        const project = await Project.findById(projectId)
-
-        if(!project){
-            const error = new Error('Project not found')
-            res.status(404).json({error: error.message})
-            return
-        }
-
         try {
             const task = new Task(req.body)
-            task.project = project.id
-            project.tasks.push(task.id)
+            task.project = req.project.id
+            req.project.tasks.push(task.id)
             await task.save()
-            await project.save()
+            await req.project.save()
             res.send('Task created successfully')
         } catch (error) {
-            console.error('Error al crear la tarea:', error);
+            console.error('Error creating the task: ', error);
         }
     };
 }
