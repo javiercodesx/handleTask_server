b2bc5d1dd746314848c2270fea7b3a69e39bfd6f
javxdev <javxdev@gmail.com>
2024-10-15 18:29:31 -0300

    add getProjectTasks

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -12,5 +12,15 @@ export class TaskController {
         } catch (error) {
             console.error('Error creating the task: ', error);
         }
-    };
+    }
+
+    static getProjectTasks = async (req: Request, res: Response) => {
+        try {
+            const tasks = await Task.find({project: req.project.id})
+            res.json(tasks)
+        } catch (error) {
+            res.status(500).json({error: 'Theres a new error'})
+        }
+    }
+
 }
