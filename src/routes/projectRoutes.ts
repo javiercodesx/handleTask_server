19cd6471331f824065219f8b8302d2bda19a9025
javxdev <javxdev@gmail.com>
2024-10-19 19:22:55 -0300

    add getTaskById

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -65,4 +65,9 @@ router.get('/:projectId/tasks',
     TaskController.getProjectTasks
 )
 
+router.get('/:projectId/tasks/:taskId',
+    validateProjectExists,
+    TaskController.getTaskById
+)
+
 export default router
