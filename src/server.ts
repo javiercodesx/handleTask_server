c3fec2a0802703625c9981cea04f3bbb1d302aa7
javxdev <javxdev@gmail.com>
2024-11-13 17:22:21 -0300

    add CorsOptions

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express from 'express'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
+import cors, { CorsOptions } from 'cors'
 
 connectDB()
 
 const app = express()
+
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        const whiteList = [process.env.FRONTEND_URL]
+        
+        if(whiteList.includes(origin)){
+            callback(null, true)
+        } else {
+            callback(new Error('Cors ERROR'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
