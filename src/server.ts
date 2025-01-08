aa3622f320ea30d568fd1e5b9e1354a6fe4dd6f5
javxdev <javxdev@gmail.com>
2025-01-08 11:19:53 -0300

    add authRoute, script dev:api

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { connectDB } from './config/db'
+import authRoutes from './routes/authRoutes'
 import projectRoutes from './routes/projectRoutes'
 import morgan from 'morgan'
 import cors, { CorsOptions } from 'cors'
@@ -11,6 +12,10 @@ const app = express()
 const corsOptions : CorsOptions = {
     origin: function(origin, callback) {
         const whiteList = [process.env.FRONTEND_URL]
+        
+        if(process.argv[2] == '--api'){
+            whiteList.push(undefined)
+        }
         
         if(whiteList.includes(origin)){
             callback(null, true)
@@ -29,6 +34,7 @@ app.use(morgan('dev'))
 // Read JSON format
 app.use(express.json())
 
+app.use('/api/auth', authRoutes)
 app.use('/api/projects', projectRoutes)
 
 export default app
