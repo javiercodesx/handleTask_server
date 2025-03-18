bcb03cd3fef3f40e4fefd8ed3774cdba0bb73897
Javier <javxdev@gmail.com>
2025-03-18 15:36:26 -0300

    update Token model from createdAt to expiresAt

--- src/models/Token.ts
+++ src/models/Token.ts
@@ -3,13 +3,13 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 export interface IToken extends Document {
     token: string,
     user: Types.ObjectId,
-    createdAt: Date
+    expiresAt: Date
 }
 
 const tokenSchema: Schema = new Schema({
     token: { type: String, required: true },
     user: { type: Types.ObjectId, ref: 'User' },
-    createdAt: { type: Date, default: Date.now(), expires: "10m" }
+    expiresAt: { type: Date, default: Date.now(), expires: "10m" }
 }, { timestamps: true })
 
 const Token = mongoose.model<IToken>('Token', tokenSchema)
