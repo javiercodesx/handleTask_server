e6a0179d0f6ce812069ab164f6289a69b3330e2a
javier <javierbk9@gmail.com>
2025-06-21 17:17:12 -0300

    chore: add static typing to the validation middleware

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,14 +1,18 @@
-import type { NextFunction, Request, Response } from 'express'
-import { validationResult } from "express-validator"
+import type { NextFunction, Request, Response } from "express";
+import { Result, validationResult } from "express-validator";
+import type { ValidationError } from "express-validator";
 
-export const handleInputErrors = (req : Request , res : Response, next : NextFunction) => {
+export const handleInputErrors: (req: Request, res: Response, next: NextFunction) => void = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
 
-    let errors = validationResult(req)
+    let errors: Result<ValidationError> = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        res.status(400).json({errors: errors.array()})
-        return
-    }
-    
-    next()
-} 
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    };
+
+    next();
+};
