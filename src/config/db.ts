96aa95c1251d7bcdef18f47d6dde5e3dab150950
Javier <javxdev@gmail.com>
2025-07-26 14:44:10 -0300

    formatting

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose'
-import 'dotenv/config'
-import color from 'colors'
+import mongoose from 'mongoose';
+import type { Connection } from 'mongoose';
+import 'dotenv/config';
+import color from 'colors';
 
-export const connectDB = async () => {
+export const connectDB = async (): Promise<void> => {
     try {
-        const {connection} = await mongoose.connect(process.env.DATABASE_URL)
-        const url = `${connection.host}:${connection.port}`
-        console.log(color.magenta.bold(`Connected to ${url}`))
+        const { connection }: { connection: Connection } = await mongoose.connect(process.env.DATABASE_URL || '');
+        const url: string = `${connection.host}:${connection.port}`;
+        console.log(color.magenta.bold(`Connected to ${url}`));
     } catch (error) {
-        console.log(color.red.bold((' Error trying to connect to the DB ')))
-        process.exit(1)
-    }
-}
+        console.log(color.red.bold((' Error trying to connect to the DB ')));
+        process.exit(1);
+    };
+};
--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,44 @@
-import express from 'express'
-import { connectDB } from './config/db'
-import authRoutes from './routes/authRoutes'
-import projectRoutes from './routes/projectRoutes'
-import morgan from 'morgan'
-import cors, { CorsOptions } from 'cors'
-import 'dotenv/config'
+import express from 'express';
+import type { Express } from 'express';
+import { connectDB } from './config/db';
+import authRoutes from './routes/authRoutes';
+import projectRoutes from './routes/projectRoutes';
+import morgan from 'morgan';
+import cors from 'cors';
+import type { CorsOptions } from 'cors';
+import 'dotenv/config';
 
-connectDB()
+connectDB();
 
-const app = express()
+const app: Express = express();
 
-const corsOptions : CorsOptions = {
-    origin: function(origin, callback) {
-        const whiteList = [process.env.FRONTEND_URL]
-        
-        if(process.argv[2] == '--api'){
-            whiteList.push(undefined)
-        }
-        
-        if(whiteList.includes(origin)){
-            callback(null, true)
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        const whiteList: (string | undefined)[] = [process.env.FRONTEND_URL];
+
+        if (process.argv[2] == '--api') {
+            whiteList.push(undefined);
+        };
+
+        if (whiteList.includes(origin)) {
+            callback(null, true);
         } else {
-            callback(new Error('Cors ERROR'))
-        }
-    }
-}
+            callback(new Error('Cors ERROR'));
+        };
+    },
+};
 
 // Cors
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // Logging
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // Read JSON format
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes)
-app.use('/api/projects', projectRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
 
-export default app
+export default app;
